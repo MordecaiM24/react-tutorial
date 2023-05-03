6e104a80432bc760c003e4475e86ebb582198e33
mordecai <mordecai.mengesteab@gmail.com>
2023-05-02 21:04:47 -0400

    Learned props, basic modularity

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Job salary={90000} position="Senior SDE" company="Amazon"/>
+      <Job salary={10000} position="Junior SDE" company="Google"/>
+      <Job salary={12000} position="Project Manager" company="Netflix"/>
+
+    </div>
+  );
+}
+
+const Job = (props) => {
+  return (
+    <div>
+      <h1>{props.position}</h1>
+      <h2>{props.salary}</h2>
+      <h2>{props.company}</h2>
     </div>
   );
 }
