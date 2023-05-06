1f99f52e8a4a8517e307b0c9eab126d189a9c011
Mordecai <mordecai.mengesteab@gmail.com>
2023-05-06 09:09:54 -0400

    Partway through todolist

--- src/App.js
+++ src/App.js
@@ -2,24 +2,44 @@ import './App.css';
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-  
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  }
+
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : ([todoList.length-1].id),
+      taskName: newTask
+    }
+    setTodoList([...todoList, task]);
+  }
+
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter(task => task.id !== id));
+  }
 
   return (
     <div className="App">
-      <button onClick={() => {
-        setCount(count + 1)
-      }}>Increase</button>
-      
-      <button onClick={() => {
-        setCount(count - 1)
-      }}>Decrease</button>
 
-      <button onClick={() => {
-        setCount(0)
-      }}>Set to Zero</button>
+      <div className="addTask">
+        <input onChange={handleChange}/>
+        <button onClick={addTask}> Add Task </button>
+      </div>
 
-      {count}
+      <div className="list">
+        {todoList.map((task) => {
+          return (
+            <div>
+              <h1>{task.taskName} {task.id}</h1>
+              <button onClick={() => deleteTask(task.id)}>X</button>
+            </div>
+          );
+        })}
+      </div>
+
     </div>
   );
 }
